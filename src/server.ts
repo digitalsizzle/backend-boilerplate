b9353997c4b74f787f5aee3db5f427061ba66789
juno <juno@junos-MacBook-Air.local>
2018-03-11 23:36:09 -0700

    Starting point

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+const x = 5;
+const y = 6;
+
+const add = (a: number, b: number): number => {
+    return a + b;
+};
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
+};
+
+console.log(add(x, y));
+
+console.log(multiply(x, y));
