ece1224b622ae391fd4f3d38dd0f54c78ffd76e0
juno <juno@junos-MacBook-Air.local>
2018-03-12 01:09:21 -0700

    Working discord bot

---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    discord: {
+        token: 'NDIxMjEzMzg5MTE3OTgwNjcz.DYfEnA.vKC2vNgLVSbsgS5LC4VSoiD0BiU',
+    },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
-const x = 5;
-const y = 6;
+import Discord from 'discord.io';
 
-const add = (a: number, b: number): number => {
-    return a + b;
-};
+import { config } from './config/config';
 
-const multiply = (a: number, b: number): number => {
-    return a * b;
-};
+process.on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+});
 
-console.log(add(x, y));
+const bot = new Discord.Client({
+    token: config.discord.token,
+    autorun: true,
+});
 
-console.log(multiply(x, y));
+bot.on('ready', () => {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', (userName: string, userId: string, channelId: string, message: string, event: any) => {
+    if (message === 'ping') {
+        bot.sendMessage({
+            to: channelId,
+            message: 'pong',
+        });
+    }
+});
